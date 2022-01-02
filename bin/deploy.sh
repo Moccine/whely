c54cc9634f740832050076d7e71f2a52be59f41d
msow <msow@kernix.com>
2022-01-02 12:15:09 +0100

    update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,17 +9,16 @@ git pull origin $(git rev-parse --abbrev-ref HEAD)
 #ssh-add ~/.ssh/github-3dtech
 echo -e 'Download updates'
 echo -e 'Installing Symfony dependencies'
- cp -r /var/www/bmsconsulting-gn.com/app/symfony/public/bundle3/fosckeditor /var/www/bmsconsulting-gn.com/app/symfony/public/bundles
-cd /var/www/bmsconsulting-gn.com/app/symfony
+cd /var/www/whely/whely/app/symfony
 composer install
 php bin/console doctrine:migrations:migrate --no-interaction
-cd /var/www/bmsconsulting-gn.com/app/integration/ && yarn run start
+cd /var/www/whely/whely/app/integration/ && yarn run start
 echo -e 'Installation of assets'
 php bin/console assets:install public
 echo -e 'Installation of Front dependencies'
 cd /var/www/bmsconsulting-gn.com/app/integration/ && yarn run encore production
 
-cd /var/www/bmsconsulting-gn.com/app/symfony
+cd /var/www/whely/whely/app/symfony
 echo -e 'Clear du cache'
 php bin/console cache:clear
 php bin/console cache:warmup
